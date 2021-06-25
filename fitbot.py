32bf087129a8e3890c53cd00ca219b5dd9db8a9f
Josiah Smith <josiah.smith@instacart.com>
2021-06-24 23:01:21 -0700

    add columns, commas, and check yesterday's date

--- fitbot.py
+++ fitbot.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+from datetime import timedelta
 import os
 import pytz
 from decouple import config
@@ -16,24 +17,37 @@ def main():
 	sup_access_token = config('SUP_ACCESS_TOKEN')
 	rishi_access_token = config('RISHI_ACCESS_TOKEN')
 
-	date = get_current_date()
+	access_tokens = [josiah_access_token, kartik_access_token, sup_access_token, rishi_access_token]
 
+
+	yesterday = get_current_date() - timedelta(days = 1)
+	formatted_date = yesterday.strftime('%Y-%m-%d')
 
 	webhook = Webhook.from_url(discord_url, adapter=RequestsWebhookAdapter())
 	e = discord.Embed(
-		title="Daily Fitbot " + date.strftime("%x"), 
+		title="Daily Fitbot " + yesterday.strftime("%x"), 
 		color=65520, 
 		description="",
 		thumbnail='https://logos-world.net/wp-content/uploads/2021/02/Fitbit-Emblem.png',
 		url='https://www.fitbit.com/')
-	e.add_field(name="__Fitbit Leaderboard__", value=get_leaderboard(josiah_access_token), inline=True)
-	e.add_field(name="__Daily Step Counts__", value=get_daily_steps_leaderboard([josiah_access_token, kartik_access_token, sup_access_token, rishi_access_token]), inline=True)
+
+	weekly_names_value, weekly_steps_value = get_leaderboard(josiah_access_token)
+	e.add_field(name="__Weekly Steps__", value=weekly_names_value, inline=True)
+	e.add_field(name='\u200b', value='\u200b', inline=True) # strictly for formatting
+	e.add_field(name='⠀', value=weekly_steps_value, inline=True)
+
+	
+
+	daily_names_value, daily_steps_value = get_daily_steps_leaderboard(access_tokens, formatted_date)
+	e.add_field(name="__Yesterday's Steps__", value=daily_names_value, inline=True)
+	e.add_field(name='\u200b', value='\u200b', inline=True) # strictly for formatting
+	e.add_field(name='⠀', value=daily_steps_value, inline=True)
+
+
 	e.set_thumbnail(url='https://logos-world.net/wp-content/uploads/2021/02/Fitbit-Emblem.png')
 
 
 	webhook.send(embed=e)
-
-
 
 def get_leaderboard(access_token):
 	header = {'Authorization': 'Bearer {}'.format(access_token)}
@@ -45,36 +59,42 @@ def get_leaderboard(access_token):
 	for person in response['included']:
 	    id_hash[person['id']] = person['attributes']['name']
 
-	date = get_current_date()
 
-
-	final_str = '\n'
+	names_str = '\n'
+	steps_str = '\n'
 	for data in response['data']:
 		user_id = data['relationships']['user']['data']['id']
 		rank = data['attributes']['step-rank'] 
 		step_count = data['attributes']['step-summary'] 
-		append_str = str(rank) + ". " + id_hash[user_id] + " - " + str(int(step_count))
-		final_str += append_str + '\n'
-	return final_str
 
+		names_str += str(rank) + ". " + id_hash[user_id].replace('.', '') + ":"
+		names_str += '\n'
+		steps_str += f'{int(step_count):,}'
+		steps_str += '\n'
 
-def get_daily_steps_leaderboard(access_tokens):
-	formatted_date = get_current_date().strftime('%Y-%m-%d')
+	return names_str, steps_str
 
+# date must be in YYYY-MM-DD format
+def get_daily_steps_leaderboard(access_tokens, date):
+	# hash from number of steps to name
 	steps_hash = {}
 	for access_token in access_tokens:
 		header = {'Authorization': 'Bearer {}'.format(access_token)}
-		response = requests.get("https://api.fitbit.com/1/user/-/activities/date/" + formatted_date + ".json", headers=header).json()
+		response = requests.get("https://api.fitbit.com/1/user/-/activities/date/" + date + ".json", headers=header).json()
 		steps_hash[get_daily_steps(response)] = get_full_name(access_token)
 
-	final_str = '\n'
+	names_str = '\n'
+	steps_str = '\n'
 	i = 1
-	for daily_step_count in sorted(steps_hash.keys(), reverse=True):
-		append_str = str(i) + ". " + steps_hash[daily_step_count] + " - " + str(daily_step_count)
-		final_str += append_str + '\n'
-		i+=1
-	return final_str
 
+	# sort hash by key (step number) 
+	for daily_step_count in sorted(steps_hash.keys(), reverse=True):
+		names_str += str(i) + ". " + steps_hash[daily_step_count].replace('.', '') + ":"
+		names_str += '\n'
+		steps_str += f'{daily_step_count:,}'
+		steps_str += '\n'
+		i+=1
+	return names_str, steps_str
 
 
 def get_daily_steps(response):
